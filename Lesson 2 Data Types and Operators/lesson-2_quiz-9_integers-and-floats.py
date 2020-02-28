f36810c7377d2ecda29a63e27e353550c864453e
Jhonatan M. Nagasako <jhonatannagasako@gmail.com>
2020-02-27 20:10:37 -0500

    Learned about floats and integers

---
+++ Lesson 2 Data Types and Operators/lesson-2_quiz-9_integers-and-floats.py
@@ -0,0 +1,33 @@
+'''
+Jhonatan Nagasako
+24-FEB-2020
+Udacity intro to Python
+Lesson 2 - Quiz 9
+Floats and integers
+'''
+
+# what happens if you divide a numbers by zero!
+
+print(5/0)
+
+'''
+SOLUTION
+Traceback means "What was the programming doing
+when it broke"! This part is usually less helpful
+than the very last line of your error. Though you
+can dig through the rest of the error, looking at
+just the final line ZeroDivisionError, and the
+message says we divided by zero. Python is
+enforcing the rules of arithmetic!
+
+In general, there are two types of errors to look
+out for
+
+- Exceptions
+- Syntax
+
+An Exception is a problem that occurs when the
+code is running, but a 'Syntax Error' is a
+problem detected when Python checks the code
+before it runs it. 
+'''
